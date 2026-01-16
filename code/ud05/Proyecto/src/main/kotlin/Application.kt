db8111f1c44a9882f710f2b44ef1cd3b86f008e1
José Abad López <jm.abadlopez@edu.gva.es>
2026-01-16 16:56:29 +0100

    Despliegue y arquitectura

--- code/ud05/Proyecto/src/main/kotlin/Application.kt
+++ code/ud05/Proyecto/src/main/kotlin/Application.kt
@@ -6,14 +6,13 @@ import edu.gva.es.plugins.configureSerialization
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
-    // 1. MODELO: Inicializamos la persistencia antes de arrancar la red
-    ConexionDB.conectar()
-
-    // 2. CONTROLADOR: Lanzamos el motor Netty de Ktor
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+
+    ConexionDB.conectar()
+
     configureSerialization()
     configureSecurity()      // INSTALACIÓN DEL GUARDIÁN (Auth y Sesiones)
     configureRouting()
