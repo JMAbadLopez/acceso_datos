b6e58e61dfba187dd2fc20143c206b1b9ac9260b
José Abad López <jm.abadlopez@edu.gva.es>
2026-01-26 11:53:06 +0100

    Resolviendo errores ud02

--- code/ud02/ProyectoUnidad02/build.gradle.kts
+++ code/ud02/ProyectoUnidad02/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.2.0"
-    kotlin("plugin.serialization") version "1.9.0"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
 }
 
 group = "edu.jmabadlopez"
@@ -18,7 +18,7 @@ dependencies {
     implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
 kotlin {
